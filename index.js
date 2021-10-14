f9acd3f57c17d36118eec6d4c545e7367521d636
VladVinichenko <vinnyxxxl925@gmail.com>
2021-10-14 04:30:21 +0300

    Prettier

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const galleryCollection = document.querySelector(".js-gallery");
-const galleryModalBox = document.querySelector(".js-lightbox");
-const modalImage = galleryModalBox.querySelector(".lightbox__image");
+const galleryCollection = document.querySelector('.js-gallery');
+const galleryModalBox = document.querySelector('.js-lightbox');
+const modalImage = galleryModalBox.querySelector('.lightbox__image');
 
-import { galleryItems } from "./app.js";
+import { galleryItems } from './app.js';
 
 const cardsGallery = createGalleryList(galleryItems);
 
-function createGalleryList(galleryItems) { //create HTML li
+function createGalleryList(galleryItems) {
+  //create HTML li
   return galleryItems
     .map(
       ({ preview, original, description }) =>
@@ -20,114 +21,114 @@ function createGalleryList(galleryItems) { //create HTML li
           data-source="${original}"
           alt="${description}"/>
       </a>
-    </li>`
+    </li>`,
     )
-    .join("");
+    .join('');
 }
 
-galleryCollection.insertAdjacentHTML("beforeend", cardsGallery);
+galleryCollection.insertAdjacentHTML('beforeend', cardsGallery);
 
 function openModalWindow() {
-  galleryModalBox.classList.add("is-open");     //open modal
-  galleryModalBox.addEventListener("click", modalCloseBtn);
-  galleryModalBox.addEventListener("click", modalCloseOverlay);
-  window.addEventListener("keydown", modalCloseEcsKey);
-  window.addEventListener("keydown", modalArrows);
+  galleryModalBox.classList.add('is-open'); //open modal
+  galleryModalBox.addEventListener('click', modalCloseBtn);
+  galleryModalBox.addEventListener('click', modalCloseOverlay);
+  window.addEventListener('keydown', modalCloseEcsKey);
+  window.addEventListener('keydown', modalArrows);
 }
 
 function closeModalWindow() {
-  galleryModalBox.classList.remove("is-open");    //close modal
-  galleryModalBox.removeEventListener("click", modalCloseBtn);
-  galleryModalBox.removeEventListener("click", modalCloseOverlay);
-  window.removeEventListener("keydown", modalCloseEcsKey);
-  window.removeEventListener("keydown", modalArrows);
+  galleryModalBox.classList.remove('is-open'); //close modal
+  galleryModalBox.removeEventListener('click', modalCloseBtn);
+  galleryModalBox.removeEventListener('click', modalCloseOverlay);
+  window.removeEventListener('keydown', modalCloseEcsKey);
+  window.removeEventListener('keydown', modalArrows);
   removeSrcImage();
 }
 
-galleryCollection.addEventListener("click", (evt) => {
+galleryCollection.addEventListener('click', evt => {
   evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
+  if (evt.target.nodeName !== 'IMG') {
     return;
   }
-  openModalWindow();    //open modal
-  imageNumber(evt);     //render select image in modal window
+  openModalWindow(); //open modal
+  imageNumber(evt); //render select image in modal window
 });
 
 function modalCloseBtn(evt) {
-  if (evt.target.className === "lightbox__button") {
-    closeModalWindow();   //close modal
+  if (evt.target.className === 'lightbox__button') {
+    closeModalWindow(); //close modal
   }
 }
 
 function modalCloseOverlay(evt) {
-  if (evt.target.className === "lightbox__overlay") {
-    closeModalWindow();   //close modal
+  if (evt.target.className === 'lightbox__overlay') {
+    closeModalWindow(); //close modal
   }
 }
 
 function modalCloseEcsKey(evt) {
-  if (evt.code === "Escape") {
-    closeModalWindow();   //close modal
+  if (evt.code === 'Escape') {
+    closeModalWindow(); //close modal
   }
 }
-const galleryImagesArray = document.querySelectorAll(".gallery__image");    //array with all links images
+const galleryImagesArray = document.querySelectorAll('.gallery__image'); //array with all links images
 
 const currentNumberImage = {
-  current: 0,   //selected image
+  current: 0, //selected image
   min: 0,
-  max: galleryImagesArray.length - 1,  //numbers images in app.js
+  max: galleryImagesArray.length - 1, //numbers images in app.js
   nextImage: function () {
     if (this.current < this.max) {
-      this.current += 1;    //next image
+      this.current += 1; //next image
       return;
     }
-    this.current = this.min;  //next image on the next round
+    this.current = this.min; //next image on the next round
     return;
   },
 
   prevImage: function () {
     if (this.current > this.min) {
-      this.current -= 1;      //prev image
+      this.current -= 1; //prev image
       return;
     }
-    this.current = this.max;      //prev image on the next round
+    this.current = this.max; //prev image on the next round
     return;
   },
 };
 
-function imageNumber(img) {    
+function imageNumber(img) {
   galleryImagesArray.forEach((e, number) => {
     if (e === img.target) {
       currentNumberImage.current = number;
-      buildImageInModal(number);      //render image with selected number image in array
+      buildImageInModal(number); //render image with selected number image in array
     }
   });
 }
 
 function buildImageInModal(img) {
-  modalImage.setAttribute("src", galleryItems[img].original);
-  modalImage.setAttribute("alt", galleryItems[img].description);
+  modalImage.setAttribute('src', galleryItems[img].original);
+  modalImage.setAttribute('alt', galleryItems[img].description);
 }
 
 function removeSrcImage() {
-  modalImage.setAttribute("src", "");
-  modalImage.setAttribute("alt", "");
+  modalImage.setAttribute('src', '');
+  modalImage.setAttribute('alt', '');
 }
 
 function nextImage() {
   currentNumberImage.nextImage();
-  buildImageInModal(currentNumberImage.current);      //render image with new "current" value
+  buildImageInModal(currentNumberImage.current); //render image with new "current" value
 }
 
 function prevImage() {
   currentNumberImage.prevImage();
-  buildImageInModal(currentNumberImage.current);      //render image with new "current" value
+  buildImageInModal(currentNumberImage.current); //render image with new "current" value
 }
 
 function modalArrows(evt) {
-  if (evt.code === "ArrowRight") {
+  if (evt.code === 'ArrowRight') {
     nextImage();
-  } else if (evt.code === "ArrowLeft") {
+  } else if (evt.code === 'ArrowLeft') {
     prevImage();
   }
 }
